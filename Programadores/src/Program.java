e446248594fae4488737fe8ec8b499d2e4825457
Adrián Balboa Tomé <a25adrianbt@teis.intranet>
2025-10-08 20:57:12 +0200

    Acabado

--- Programadores/src/Program.java
+++ Programadores/src/Program.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Program {
@@ -5,10 +6,52 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Programador Pedro = new Programador("Pedro",22000,"java","python");
+        Programador programador = new Programador("Adrián",22000,"java","python");
 
-        System.out.println("Bienvenido, que deseas hacer: \n1:Actualizar salario\n2:Actualizar lenguaje\n3:Mostrar datos");
-        Scanner sc = new Scanner(System.in);
+        int option = 1;
 
+        while (option != 4){
+            String LenguajePrincipalOriginal = programador.getLenguajePrincipal();
+            System.out.println("\nBienvenido, "+ programador.getName()+ " que deseas hacer: \n1:Actualizar salario\n2:Actualizar lenguaje\n3:Mostrar datos\n4:Salir del programa");
+            Scanner sc = new Scanner(System.in);
+            option = sc.nextInt();
+
+
+            if (option == 1){
+
+                System.out.println("Introduce tu nuevo salario (no puede ser menor al actual): ");
+                Scanner sc1 = new Scanner(System.in);
+                int getSalario = sc.nextInt();
+                programador.setSalario(getSalario);
+
+
+            } else if (option == 2) {
+                System.out.println("Introduce el nuevo lenguaje de programacion: ");
+                Scanner sc2 = new Scanner(System.in);
+                String newLanguage = sc2.nextLine();
+
+
+                if (Objects.equals(newLanguage, programador.getLenguajeSecundario())){
+
+                    programador.setLenguajeSecundario(programador.getLenguajePrincipal());
+                    programador.setLenguajePrincipal(newLanguage);
+
+                } else if (Objects.equals(newLanguage, programador.getLenguajePrincipal())) {
+                    System.out.println("El lenguaje introducido "+ newLanguage + " es igual al lenguaje principal "+ programador.getLenguajePrincipal());
+                } else{
+                    programador.setLenguajeSecundario(programador.getLenguajePrincipal());
+                    programador.setLenguajePrincipal(newLanguage);
+                }
+
+            } else if (option == 3) {
+
+                System.out.println("Estos son los datos almacenados: ");
+                System.out.println("Nombre: "+programador.getName());
+                System.out.println("Salario: "+programador.getSalario());
+                System.out.println("Lenguaje Principal: "+programador.getLenguajePrincipal());
+                System.out.println("Lenguaje Secundario: "+programador.getLenguajeSecundario());
+
+            }
+        }
     }
 }
--- Programadores/src/Programador.java
+++ Programadores/src/Programador.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Programador {
 
     private String name;
-    private int salario = 22000;
+    private int salario;
     private String lenguajePrincipal;
     private String lenguajeSecundario;
 
@@ -14,17 +14,18 @@ public class Programador {
     public int getSalario(){
         return salario;
     }
-    public int setSalario(int salario){
-        System.out.println("Introduce tu nuevo salario (no puede ser menor al actual): ");
-        Scanner sc = new Scanner(System.in);
-        int newSalario = sc.nextInt();
 
-        while (newSalario <= salario){
+    public void  setSalario(int salario){
+
+        int newSalario = salario;
+
+        while (newSalario <= this.salario){
             System.out.println("El salario introducido no cumple con las normas, introducelo de nuevo: ");
-            Scanner sc1 = new Scanner(System.in);
+            Scanner sc = new Scanner(System.in);
             newSalario = sc.nextInt();
         }
-        return newSalario;
+        this.salario = newSalario;
+        System.out.println("Nuevo salario actualizado correctamente.");
     }
 
     public String getLenguajePrincipal(){
@@ -35,10 +36,11 @@ public class Programador {
         return lenguajeSecundario;
     }
 
-    public String setLenguajePrincipal(){
-        System.out.println("Introduce el nuevo lenguaje: ");
-        Scanner sc = new Scanner(System.in);
-        return sc.nextLine();
+    public void  setLenguajePrincipal(String lenguajePrincipal){
+        this.lenguajePrincipal =lenguajePrincipal;
+    }
+    public void  setLenguajeSecundario(String lenguajeSecundario){
+        this.lenguajeSecundario = lenguajeSecundario ;
     }
 
     public Programador(String name, int salario, String lenguajePrincipal, String lenguajeSecundario) {
